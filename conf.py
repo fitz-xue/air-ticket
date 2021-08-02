bc47818f8f80fa7f4008959c6b739adb26e06ce9
fitz <fitzxue@gmail.com>
2021-08-02 17:52:05 +0800

    config file

---
+++ conf.py
@@ -0,0 +1,16 @@
+#amadeus config
+amadeus_user_name = 'PHONGYUAN'
+amadeus_user_pwd = '@Lagos11'
+amadeus_office_id = 'PHONGYUAN'
+amadeus_url = 'https://www.sellingplatformconnect.amadeus.com/LoginService/login.jsp?SITE=LOGINURL&LANGUAGE=GB&refreshOnError=true'
+
+# chrome config
+chromedirver_path = '/usr/local/bin/chromedriver'
+otp_path = '/Users/hbxue/Desktop/otp.txt'
+
+# Email config
+mail_host = "smtp.163.com"  # SMTP_server
+mail_user = "gtduke"  # email_user_name
+mail_pass = "JNEMUKROYBEQWGMX"  # SMTP_Token
+sender = 'gtduke@163.com'  # email_sender
+receivers = ['231337331@qq.com', 'Lois15023130051@163.com']  # email receivers
